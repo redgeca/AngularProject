8e35eb51a62ac1030e90ba424ea84495ab1e496e
Rejeean Caron <redgeca@gmail.com>
2025-01-19 11:37:25 -0500

    Removed README conten

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { MqttModule } from 'ngx-mqtt';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), MqttModule]
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+        provideAnimationsAsync(),
+    ]
 };
